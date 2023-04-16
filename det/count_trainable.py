48b012b9961ebeb93fabfca931327cf9728eb78a
Daffa <daffa.ajiputra@gmail.com>
2023-04-16 13:01:01 +0700

    Modification for count_trainable.py

--- det/count_trainable.py
+++ det/count_trainable.py
@@ -1,9 +1,14 @@
 import torch
+from torchvision import models
+from torchinfo import summary
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # Load the pre-trained model from the .pt file
-model = torch.load("runs/train/yolov7-800-AdamW/weights/last.pt")
+model = torch.load("runs/train/yolov7-800/weights/best.pt", map_location=device)["model"]
 
-arch = model['model']
-n_p = sum(x.numel() for x in arch.parameters())
+# print(model)
+# n_p = sum(x.numel() for x in arch.parameters())
 
-print(f"There are {n_p} Trainable Parameters")
+# print(f"There are {n_p} Trainable Parameters")
+
+summary(model.to(torch.float), input_size=(4,3,800,800))
